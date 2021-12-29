7b027f17f0f4ddbf1c796681e63d4b5259594969
kami011 <kamila.g2067@gmail.com>
2021-12-29 19:38:24 +0100

    Cell changes in view connected with model

--- controller.py
+++ controller.py
@@ -7,3 +7,16 @@ class Controller:
     def changeTableSize(self, height, width):
         self.table.setHeight(height)
         self.table.setWidth(width)
+        print(height, ' ', width)
+
+
+    def getTable(self, tab):
+        print(self.table.getContent())
+        for i in range(len(self.table.getContent())):
+            for j in range(len(self.table.getContent()[i])):
+                self.table.getContent()[i][j].setValue(tab[i][j])
+                print(self.table.getContent()[i][j].getValue())
+
+    
+    #def mergeCells(self, cells):
+        
